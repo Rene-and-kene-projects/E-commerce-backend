3a422a3a8c3793e1fc785c9cc0c85a2de70270fc
Rene0115 <enere0115@gmail.com>
2023-05-28 11:48:39 +0100

    fix

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,4 +1,4 @@
-import express from "expres";
+import express from "express";
 import productController from "../controllers/product.controller.js";
 import store from "../config/multer.config.js";
 
